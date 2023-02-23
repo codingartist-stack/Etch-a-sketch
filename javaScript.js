0f2c1e0287134b11d3a059d8a6ed9d2d35d2563b
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-02-22 20:19:14 -0700

    still trying to figure out how to change the color of box when hovering over it.

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,11 @@
 const grid = document.getElementById('squareGrid');
 
+
 for (let i = 0; i < 16; i++) {
     const gridBox = document.createElement('div');
-    gridBox.classList.add('box')
+    gridBox.classList.add('box');
     squareGrid.appendChild(gridBox);
 }
-
-addEventListener('mouseover', (e) => {
-    
-})
+// gridBox.addEventListener("mouseover", (event) => {
+    // gridBox.classList.add('boxHover');
+// });
