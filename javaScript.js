57fc5d4b59de1004d336624483205f6578edf338
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-02-22 20:57:46 -0700

    can't figure out how to grab the classname for each box that is created via js. will leave it for tomorrow.

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,11 @@
 const grid = document.getElementById('squareGrid');
 
-
 for (let i = 0; i < 16; i++) {
     const gridBox = document.createElement('div');
     gridBox.classList.add('box');
     squareGrid.appendChild(gridBox);
 }
-// gridBox.addEventListener("mouseover", (event) => {
-    // gridBox.classList.add('boxHover');
-// });
+
+
+
+
