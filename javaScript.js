a2c7e88487b71af90d416648af43d9c657caeee5
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-02-22 21:50:24 -0700

    nested a loop to create grid added a user input option. trying to connect them.

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,15 @@
 const grid = document.getElementById('squareGrid');
+const userInput = document.getElementById('width-height');
 
-for (let i = 0; i < 16; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.classList.add('box');
-    squareGrid.appendChild(gridBox);
+const button = document.getElementById('button')
+
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add(`box`);
+        cell.setAttribute('id', `${i+1}_${j+1}`)
+        grid.appendChild(cell);
+    }
 }
 
 
